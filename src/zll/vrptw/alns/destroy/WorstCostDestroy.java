e1cfcc546e2bff1248253e06e42997fcd9649daf
younger <45989017+younger-1@users.noreply.github.com>
2020-08-21 11:26:39 +0800

    1. 把原来 WorstCostDestroy 类中 destroy 算子的移除节点带来的【成本下降】的计算做了纠正 2. 节点移除方法从一个个移除改为一次移除所有 worst 节点

--- src/zll/vrptw/alns/destroy/WorstCostDestroy.java
+++ src/zll/vrptw/alns/destroy/WorstCostDestroy.java
@@ -2,10 +2,11 @@ package zll.vrptw.alns.destroy;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 
 import zll.vrptw.algrithm.MyALNSSolution;
 import zll.vrptw.alns.operation.ALNSAbstractOperation;
-import zll.vrptw.instance.Node;
 import zll.vrptw.instance.Route;
 import zll.vrptw.instance.Instance;
 
@@ -18,7 +19,7 @@ import zll.vrptw.instance.Instance;
  * </p>
  * 
  * @author zll_hust
- * @date 2020Äê3ÔÂ19ÈÕ
+ * @date 2020å¹´3æœˆ19æ—¥
  */
 public class WorstCostDestroy extends ALNSAbstractOperation implements IALNSDestroy {
 	/*
@@ -33,60 +34,77 @@ public class WorstCostDestroy extends ALNSAbstractOperation implements IALNSDest
 			return s;
 		}
 
-		// ¼ÆËãfitnessÖµ£¬¶Ô¿Í»§½øÐÐÆÀ¹À¡£
-		ArrayList<Fitness> customerFitness = new ArrayList<Fitness>();
-		for (Route route : s.routes) {
-			for (Node customer : route.getRoute()) {
-				double fitness = Fitness.calculateFitness(s.instance, customer, route);
-				customerFitness.add(new Fitness(customer.getId(), fitness));
-			}
-		}
-
-		Collections.sort(customerFitness);
-
-		ArrayList<Integer> removal = new ArrayList<Integer>();
-		for (int i = 0; i < removeNr; ++i)
-			removal.add(customerFitness.get(i).customerNo);
-
+		// !new
+		ArrayList<Fitness> customList = new ArrayList<>();
 		for (int j = 0; j < s.routes.size(); j++) {
-			for (int i = 0; i < s.routes.get(j).getRoute().size(); ++i) {
-				Node customer = s.routes.get(j).getRoute().get(i);
-				if (removal.contains(customer.getId())) {
-					s.removeCustomer(j, i);
-				}
+			for (int i = 1; i < s.routes.get(j).getRoute().size() - 1; ++i) {
+				customList.add(Fitness.calculateFitness(s.instance, s.routes.get(j), j, i));
 			}
 		}
 
+		// ! è®°å½• customList ä¸­ fitness æœ€å¤§çš„å‰ removeNr ä¸ªçš„åºå·ï¼ŒæŒ‰åºå·ä»Žå¤§åˆ°å°æŽ’åˆ—
+		HashMap<Fitness, Integer> hash = new HashMap<>();
+		for (int k = 0; k < customList.size(); k++) {
+			hash.put(customList.get(k), k);
+		}
+
+		ArrayList<Fitness> customListCopy = new ArrayList<>(customList);
+		Collections.sort(customListCopy);
+
+		ArrayList<Integer> removeList = new ArrayList();
+		for (int i = 0; i < removeNr; i++) {
+			removeList.add(hash.get(customListCopy.get(i)));
+		}
+
+		Collections.sort(removeList, new Comparator<Integer>() {
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+
+		});
+		System.out.println(removeList);
+		// ç§»é™¤
+		for (int i = 0; i < removeNr; i++) {
+			s.removeCustomer(customList.get(removeList.get(i)).routeNo, customList.get(removeList.get(i)).customerNo);
+		}
 		return s;
 	}
 }
 
 class Fitness implements Comparable<Fitness> {
+	public int routeNo;
 	public int customerNo;
 	public double fitness;
 
 	public Fitness() {
 	}
 
-	public Fitness(int cNo, double f) {
-		customerNo = cNo;
-		fitness = f;
-	}
-
-	public static double calculateFitness(Instance instance, Node customer, Route route) {
+	public static Fitness calculateFitness(Instance instance, Route a_Route, int rNo, int cNo) {
 		double[][] distance = instance.getDistanceMatrix();
 
-		double fitness = (route.getCost().getTimeViolation() + route.getCost().getLoadViolation()
-				+ customer.getDemand())
-				* (distance[customer.getId()][route.getRoute().get(0).getId()]
-						+ distance[route.getRoute().get(0).getId()][customer.getId()]);
+		// ! why
+		// int x = route.getRoute().get(0).getId();
+		// int y = customer.getId();
+		// double fitness = (route.getCost().getTimeViolation() +
+		// route.getCost().getLoadViolation()
+		// + customer.getDemand()) * (distance[y][x] + distance[x][y]);
 
-		return fitness;
+		// ! new
+		Fitness f = new Fitness();
+
+		double cost_down = distance[a_Route.getRoute().get(cNo - 1).getId()][a_Route.getRoute().get(cNo).getId()]
+				+ distance[a_Route.getRoute().get(cNo).getId()][a_Route.getRoute().get(cNo + 1).getId()]
+				- distance[a_Route.getRoute().get(cNo - 1).getId()][a_Route.getRoute().get(cNo + 1).getId()];
+		f.fitness = cost_down;
+
+		f.routeNo = rNo;
+		f.customerNo = cNo;
+		f.fitness = cost_down;
+		return f;
 	}
 
 	@Override
-	public int compareTo(Fitness o) {
-		Fitness s = (Fitness) o;
+	public int compareTo(Fitness s) {
 		if (s.fitness > this.fitness) {
 			return 1;
 		} else if (this.fitness == s.fitness) {
