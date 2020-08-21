b69a87c145eae72dd15981a5e5547af2f0888796
younger <45989017+younger-1@users.noreply.github.com>
2020-08-21 20:26:01 +0800

    **HashMap取下标** 改为 只使用`int[]`，减少了空间消耗并提升了性能 - 用于一次移除所有 worst 节点

--- src/zll/vrptw/alns/destroy/WorstCostDestroy.java
+++ src/zll/vrptw/alns/destroy/WorstCostDestroy.java
@@ -1,9 +1,10 @@
 package zll.vrptw.alns.destroy;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 
 import zll.vrptw.algrithm.MyALNSSolution;
 import zll.vrptw.alns.operation.ALNSAbstractOperation;
@@ -43,29 +44,45 @@ public class WorstCostDestroy extends ALNSAbstractOperation implements IALNSDest
 		}
 
 		// ! 记录 customList 中 fitness 最大的前 removeNr 个的序号，按序号从大到小排列
-		HashMap<Fitness, Integer> hash = new HashMap<>();
-		for (int k = 0; k < customList.size(); k++) {
-			hash.put(customList.get(k), k);
-		}
+		// removeList 里放 customList 中 fitness 最大的前 removeNr 个的下标
+		// 每次外循环替换掉 removeList 里 fitness 最小的下标
 
-		ArrayList<Fitness> customListCopy = new ArrayList<>(customList);
-		Collections.sort(customListCopy);
-
-		ArrayList<Integer> removeList = new ArrayList();
+		int[] removeList = new int[removeNr];
 		for (int i = 0; i < removeNr; i++) {
-			removeList.add(hash.get(customListCopy.get(i)));
+			removeList[i] = i;
 		}
 
-		Collections.sort(removeList, new Comparator<Integer>() {
-			public int compare(Integer o1, Integer o2) {
-				return o2 - o1;
+		for (int i = removeNr; i < customList.size(); i++) {
+			double max_cost_diff = 0;
+			int max_cost_diff_index = 0;
+			boolean is_custom_into_removeList = false;
+			for (int j = 0; j < removeNr; j++) {
+				double tmp = customList.get(i).fitness - customList.get(removeList[j]).fitness;
+				if (tmp > 0) {
+					is_custom_into_removeList = true;
+					if (tmp > max_cost_diff) {
+						max_cost_diff = tmp;
+						max_cost_diff_index = j;
+					}
+				}
 			}
+			if (is_custom_into_removeList)
+				removeList[max_cost_diff_index] = i;
+		}
 
-		});
-		System.out.println(removeList);
+		// Arrays.sort(removeList, Collections.reverseOrder());
+		// Arrays.sort(removeList, new Comparator<Integer>() {
+		// public int compare(Integer o1, Integer o2) {
+		// return o1 < o2 ? 1 : -1;
+		// }
+		// });
+		Arrays.sort(removeList);
+
+		// System.out.println(removeList);
+		// System.out.println(Arrays.toString(removeList));
 		// 移除
-		for (int i = 0; i < removeNr; i++) {
-			s.removeCustomer(customList.get(removeList.get(i)).routeNo, customList.get(removeList.get(i)).customerNo);
+		for (int i = removeNr - 1; i >= 0; i--) {
+			s.removeCustomer(customList.get(removeList[i]).routeNo, customList.get(removeList[i]).customerNo);
 		}
 		return s;
 	}
@@ -105,12 +122,12 @@ class Fitness implements Comparable<Fitness> {
 
 	@Override
 	public int compareTo(Fitness s) {
-		if (s.fitness > this.fitness) {
-			return 1;
+		if (this.fitness < s.fitness) {
+			return -1;
 		} else if (this.fitness == s.fitness) {
 			return 0;
 		} else {
-			return -1;
+			return 1;
 		}
 	}
 
