4de7fd62fd81de27c2fdbc96fbb37af87c2b5914
younger <45989017+younger-1@users.noreply.github.com>
2020-09-29 22:50:09 +0800

    #add visual api

--- src/zll/vrptw/alns/config/ControlParameter.java
+++ src/zll/vrptw/alns/config/ControlParameter.java
@@ -2,25 +2,32 @@ package zll.vrptw.alns.config;
 
 public class ControlParameter {
 
-    private final boolean solutionsLinechart;
-    private final boolean operationsLinechart;
-    private final boolean solutionImages;
+    private final boolean init_sol_chart;
+    private final boolean s1_sol_chart;
+    private final boolean s2_sol_chart;
+    private final boolean global_sol_chart;
 
-    public ControlParameter(boolean showSolutionsLinechart, boolean showOperationsLinechart, boolean createSolutionImages) {
-        solutionsLinechart = showSolutionsLinechart;
-        operationsLinechart = showOperationsLinechart;
-        solutionImages = createSolutionImages;
+    public ControlParameter(boolean init_sol_chart, boolean s1_sol_chart, boolean s2_sol_chart,
+            boolean global_sol_chart) {
+        this.init_sol_chart = init_sol_chart;
+        this.s1_sol_chart = s1_sol_chart;
+        this.s2_sol_chart = s2_sol_chart;
+        this.global_sol_chart = global_sol_chart;
     }
 
-    public boolean isSolutionsLinechart() {
-        return this.solutionsLinechart;
+    public boolean isGlobal_sol_chart() {
+        return global_sol_chart;
     }
 
-    public boolean isOperationsLinechart() {
-        return this.operationsLinechart;
+    public boolean isS2_sol_chart() {
+        return s2_sol_chart;
     }
 
-    public boolean isSolutionImages() {
-        return this.solutionImages;
+    public boolean isS1_sol_chart() {
+        return s1_sol_chart;
+    }
+
+    public boolean isInit_sol_chart() {
+        return init_sol_chart;
     }
 }
