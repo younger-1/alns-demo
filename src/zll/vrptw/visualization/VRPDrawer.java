c97da5d56ee43e33a99444a7e54ba206c2302cc7
younger <45989017+younger-1@users.noreply.github.com>
2020-10-02 10:46:11 +0800

    Visualization 2: add border and color

--- src/zll/vrptw/visualization/VRPDrawer.java
+++ src/zll/vrptw/visualization/VRPDrawer.java
@@ -2,11 +2,18 @@ package zll.vrptw.visualization;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
 import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 import zll.vrptw.algrithm.MyALNSSolution;
 import zll.vrptw.instance.Node;
@@ -21,9 +28,16 @@ public class VRPDrawer extends JPanel {
     double y_diff;
     public static void draw_sol(MyALNSSolution sol, int screenPositionIndex) {
         VRPDrawer vd = new VRPDrawer(sol);
-        JFrame myFrame = new JFrame();
-        // todo JFrame frame = new JFrame("Swing Tester");
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        String sol_kind = switch (screenPositionIndex) {
+            case 1 -> "Init Solution";
+            case 2 -> "Stage-1 Solution";
+            case 3 -> "Stage-2 Solution";
+            case 4 -> "Optimal Solution";
+            default -> "Unknow";
+        };
+        // Border myBorder = BorderFactory.createMatteBorder(5, 5, 5, 5, Color.CYAN);
+        Border myBorder = BorderFactory.createTitledBorder(sol_kind);
+        vd.setBorder(myBorder);
 
         int colNum = 2;
         int rowNum = 2;
@@ -33,14 +47,18 @@ public class VRPDrawer extends JPanel {
         int frameWidth = (int) screenSize.getWidth() / colNum;
         int frameHeight = (int) screenSize.getHeight() / rowNum;
 
+        JFrame myFrame = new JFrame("ALNS_VRP");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setBounds(xPosition * frameWidth, yPosition * frameHeight, frameWidth, frameHeight);
         myFrame.getContentPane().add(vd);
-        // todo myFrame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+        myFrame.getContentPane().add(new JPanel(), BorderLayout.EAST);
+        myFrame.getContentPane().add(new JPanel(), BorderLayout.WEST);
+        myFrame.getContentPane().add(new JPanel(), BorderLayout.NORTH);
+        myFrame.getContentPane().add(new JPanel(), BorderLayout.SOUTH);
         myFrame.setVisible(true);
     }
 
     public VRPDrawer(MyALNSSolution sol) {
-
         List<Route> routes = sol.routes;
         x_routes = new double[routes.size()][];
         y_routes = new double[routes.size()][];
@@ -50,7 +68,7 @@ public class VRPDrawer extends JPanel {
             List<Node> nodes = route.getRoute();
             x_routes[i] = new double[nodes.size()];
             y_routes[i] = new double[nodes.size()];
-            for (Node node : route.getRoute()) {
+            for (Node node : nodes) {
                 double xCord = node.getX();
                 double yCord = node.getY();
                 x_routes[i][j] = xCord;
@@ -76,11 +94,15 @@ public class VRPDrawer extends JPanel {
         int width = this.getWidth();
         int height = this.getHeight();
 
+        Random r = new Random();
+        r.setSeed(1004);
         for (int i = 0; i < x_routes.length; i++) {
             int[] x_coord = Arrays.stream(x_routes[i]).map((num) -> width * (num - x_min) / x_diff)
                     .mapToInt(num -> (int) num).toArray();
             int[] y_coord = Arrays.stream(y_routes[i]).map((num) -> height * (num - y_min) / y_diff)
                     .mapToInt(num -> (int) num).toArray();
+            Color randomColor = new Color((float) r.nextDouble(), (float) r.nextDouble(), (float) r.nextDouble());
+            g.setColor(randomColor);
             g.drawPolyline(x_coord, y_coord, x_coord.length);
         }
     }
